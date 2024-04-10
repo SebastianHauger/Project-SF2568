8697218cc263be88b9ed369ef31c63b420bce935
SebastianHauger <vogelhaugersebastian@gmail.com>
2024-04-10 16:21:30 +0200

    wrote the backtransfrom... might be bugs

--- seq_fft.c
+++ seq_fft.c
@@ -28,9 +28,18 @@ void fft(complex double  *vec, int len){
 void invFft(complex double *vec, int len){
     if (len <= 1) return;
     complex double even[len/2];
-    complex double even[len/2];
-    for (int i = 0; i < len/2; i++ )
-
+    complex double odd[len/2];
+    for (int i = 0; i < len/2; i++ ){
+        even[i] = vec[2*i];
+        odd[i] = vec[2*i + 1];
+    }
+    invFft(even, len/2);
+    invFft(odd, len/2);
+    for (int i = 0; i < len/2; i++){
+        complex double w = cexp(2.0*I*M_PI*i/len) * odd[i];
+        vec[i] = even[i] + w; 
+        vec[i + len/2] = even[i] - w; 
+    }
 }
 
 void invFft_helper(complex double *vec, int len){
@@ -49,14 +58,25 @@ int main(int argc, char **argv){
     }
     srand(time(NULL));
     int N = pow(2, atoi(argv[1]));
-    double randomVec[N];
+    double complex* randomVec = malloc(N*sizeof(double complex));
+    FILE* file1 = fopen("untouched.txt", "w");
     for (int i=0; i < N; i++){
         randomVec[i] = (double)rand() / RAND_MAX;
-        // printf("original %f\n", randomVec[i]);
+        fprintf(file1, "%f + i%f\n", creal(randomVec[i]), cimag(randomVec[i]));
     }
+    fclose(file1);
     fft(randomVec, N);
+    FILE * file2 = fopen("after_trans.txt", "w");
     for (int i = 0; i < N; i++){
-        // printf("new %f\n", randomVec[i]);
+        fprintf(file2, "%f + i%f\n", creal(randomVec[i]), cimag(randomVec[i]));
     } 
+    fclose(file2);
+    invFft_helper(randomVec, N);
+    FILE* file3 = fopen("transed_back.txt", "w");
+    for (int i = 0; i < N; i++){
+        fprintf(file3, "%f + i%f\n", creal(randomVec[i]), cimag(randomVec[i]));
+    } 
+    fclose(file3);
     exit(0);
+    free(randomVec);
 }
