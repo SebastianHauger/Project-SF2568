3f886abbd206a6e4a1cabc5d9d7fe2d203bb0329
SebastianHauger <vogelhaugersebastian@gmail.com>
2024-04-21 19:32:00 +0200

    have a running version of pfft

--- pdct.c
+++ pdct.c
@@ -28,126 +28,61 @@ this argument is the exponent of two which defines the length of the list
 
 
 void pfft(complex double *vec, int len, int localLen, int rank, int size){
-    complex double* even = malloc(len*sizeof(complex double));
-    complex double* odd = malloc(len*sizeof(complex double));
-
-
+    int tag1 = 1, tag2 = 2;
+    printf("got here 3");
+    complex double recvd, dummy;
     for (int i = log2(len)-1; i >= 0; i--){
         int fac = pow(2, i);
-        if (fac > localLen){ // communicate
-            if (rank % fac < fac) { // forward communication 
-
-            } else { // backwards communication 
-
+        for (int j = 0; j < localLen; j++){
+            printf("got here 4");
+            if (fac > localLen){ // communicate
+                if (j % 2*fac < fac) { // forward communication 
+                    MPI_Send(&vec[j], 1, MPI_C_DOUBLE_COMPLEX, rank+fac, tag1, MPI_COMM_WORLD);
+                    MPI_Recv(&recvd, 1, MPI_C_DOUBLE_COMPLEX, rank + fac, tag2, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+                    vec[j] = vec[j] + cexp(-2.0*I*M_PI*j/len) * recvd;
+                } else { // backwards communication
+                    MPI_Recv(&recvd, 1, MPI_C_DOUBLE_COMPLEX, rank - fac, tag1, MPI_COMM_WORLD, MPI_STATUS_IGNORE); 
+                    MPI_Send(&vec[j], 1, MPI_C_DOUBLE_COMPLEX, rank- fac, tag2, MPI_COMM_WORLD);
+                    vec[j] = recvd - cexp(-2.0*I*M_PI*(j-fac)/len) * vec[j];
+                }
+            } else{//no communication
+                dummy = vec[j] + cexp(-2.0*I*M_PI*(j)/len) * vec[j+ fac]; 
+                vec[j+ fac] = vec[j] - cexp(-2.0*I*M_PI*(j)/len) * vec[j+ fac];
+                vec[j] = dummy;
             }
         }
-
-
-    }
-
-}
-
-
-void fft(complex double  *vec, int len, int localLen){
-    // very basic implementation with several faults, that could damage performance for large inputs..
-    if (len <= 1) return;
-    complex double* even = malloc(len*sizeof(complex double));
-    complex double* odd = malloc(len*sizeof(complex double));
-    for (int i=0; i<len/2;i++){
-        even[i] = vec[2*i];
-        odd[i] = vec[i*2+1]; // ok because a factor of two:))
-    }
-    fft(even, len/2, localLen);
-    fft(odd, len/2, localLen);
-    for (int i = 0; i < len/2; i++) {
-        complex double w = cexp(-2.0*I*M_PI*i/len) * odd[i];
-        vec[i] = even[i] + w;
-        vec[i + len/2] = even[i] - w;
-    }
-    free(even);
-    free(odd);
-}
-
-
-void invFft(complex double *vec, int len, int localLen){
-    if (len <= 1) return;
-    double complex even[len/2];
-    double complex odd[len/2];
-    for (int i = 0; i < len/2; i++ ){
-        even[i] = vec[2*i];
-        odd[i] = vec[2*i + 1];
-    }
-    invFft(even, len/2, localLen);
-    invFft(odd, len/2, localLen);
-    for (int i = 0; i < len/2; i++){
-        complex double w = cexp(2.0*I*M_PI*i/len) * odd[i];
-        vec[i] = even[i] + w; 
-        vec[i + len/2] = even[i] - w; 
-    }
-}
-
-void invFftHelper(complex double *vec, int len, int localLen){
-    invFft(vec, len, localLen);
-    for (int i = 0; i < localLen; i++){
-        vec[i] = 1.0/len * vec[i];
     }
 }
 
 
-void dct(complex double *vec, int len, int localLen){
-    /* the discrete cosine transformation, where depending on if vec is in the frequency 
-    dimension or in the real dimension we get either frequencies or real numbers respectively*/ 
-    complex double* ftilde = malloc(localLen*sizeof(double complex));
-    for (int i = 0; i < len/2; i++){
-        ftilde[i] = vec[2*i];
-        ftilde[len-i-1] = vec[2*i+1];    
-    }
-    fft(ftilde, len, localLen);
-    for (int i=0; i < len;i++){
-        vec[i] = 2.0*creal(cexp(-I*M_PI*i/(2.0*len))*ftilde[i]);
-    }
-    vec[0] /= sqrt(2);
-    free(ftilde);
+void pIfft(complex double *vec, int len, int localLen, int rank, int size){
+
 }
 
 
-void invDct(complex double *vec, int len, int localLen){
-    complex double* ftilde = malloc(len*sizeof(double complex));
-    ftilde[0] = (1.0/sqrt(2))  * vec[0];
-    ftilde[len/2] = (1.0/sqrt(2)) * vec[len/2];
-    for (int i = 1; i < len; i++){
-        if (i != len/2){
-            ftilde[i] = 0.5*((vec[i]*cos(M_PI*i/(2*len)) + vec[len-i]*sin(M_PI*i/(2*len)))+I*(vec[i]*sin(M_PI*i/(2*len)) - vec[len-i]*cos(M_PI*i/(2*len))));
-        }
-    }
-    invFftHelper(ftilde, len, localLen);
-    for (int i=0; i < len/2;i++){
-        vec[2*i] = ftilde[i];
-        vec[2*i+1] = ftilde[len-i-1];
-    }
-    free(ftilde);
-}
 
 
 int main(int argc, char **argv){ 
     // argc is argument count and argv is a list of arguments..
-    if (argc < 3){
-        printf("Not enough input arguments");
-        exit(0);
-    }
     int rank, size;
-    int N = pow(2, atoi(argv[2]));
     MPI_Init(&argc, &argv);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+     if (argc < 2){
+        printf("Not enough input arguments");
+        exit(0);
+    }
+    int N = pow(2, atoi(argv[2]));
     srand(time(NULL));
+    printf("got here 1");
     int J = (rank < N%size) ? N/size + 1: N/size;
     double complex* randomVec = malloc(J*sizeof(double complex));
-    for (int i=0; i < N; i++){
+    for (int i=0; i < J; i++){
         randomVec[i] = (double)rand() / RAND_MAX;
     }
-    fft(randomVec, N, J);
-    MPI_Finalize();
-    exit(0);
+    printf("got here 2");
+    pfft(randomVec, N, J, rank, size);
     free(randomVec);
+    MPI_Finalize();
+    exit(0); 
 }
